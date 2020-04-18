c2f7465fcf00823170df8cf51c872a2cced72a0f
unknown <facekelvin@live.com>
2020-04-17 19:16:36 -0600

    done themes?

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ io.on("connection", function (socket) {
       }
 
       var nextColor =
-        data.color == "rgb(255, 0, 0)" ? "rgb(0, 0, 255)" : "rgb(255, 0, 0)";
+        data.color == "rgb(255, 0, 0)" ? "rgb(255, 255, 0)" : "rgb(255, 0, 0)";
 
       io.to(rooms[0]).emit("populateGameBoard", {
         array: socket.gameboard,
